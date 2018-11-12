75d355c25d61f0d5c636d0bb29c8199fa5ff45d6
unknown <huutam.ptnk@gmail.com>
2018-11-12 23:16:26 +0200

    added PageModel to pages.dart

--- lib/pages.dart
+++ lib/pages.dart
@@ -1,1 +1,27 @@
 import 'package:flutter/material.dart';
+
+final pages = [
+  new PageModel(
+    Colors.orange,
+  )
+];
+
+class Page extends StatelessWidget {
+  final PageModel viewModel;
+
+  Page({
+    this.viewModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
+
+class PageModel {
+  final color;
+  PageModel(
+    this.color,
+  );
+}
